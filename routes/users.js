c11aa0338027a6368de0bc9b516758caf333641d
Anbu302004 <anbunagarathinam30@gmail.com>
2025-12-07 11:48:04 +0530

    Profile Change

--- routes/users.js
+++ routes/users.js
@@ -3,21 +3,16 @@ import { verifyToken } from "../middleware/auth.js";
 import db from "../config/db.js";
 
 const router = express.Router();
- 
+
 router.get("/profile", verifyToken, (req, res) => {
-  const userId = req.user.id;
-
-  const sql = `SELECT u.id, u.name, u.email, u.phone_number FROM users u WHERE u.id =?`;
-
-  db.query(sql, [userId], (err, results) => {
-    if (err) return res.status(500).json({ error: err });
-
-    if (results.length === 0) {
-      return res.status(404).json({ error: "User not found" });
-    }
-
-    res.json({ profile: results[0] });
-  });
+    const userId = req.user.id;
+    db.query("SELECT id, name, email, phone_number FROM users WHERE id = ?", [userId], (err, results) => {
+      if (err) return res.status(500).json({ error: err });
+        if (results.length === 0) {
+            return res.status(404).json({ error: "User not found" });
+        }
+        res.json({ profile: results[0] });
+    });
 });
 
 export default router;
